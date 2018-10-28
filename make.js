ad628b02f0b5676b09f2bfb4455360bb9ad702dd
katrin-kot <enkor89@mail.ru>
2018-10-28 16:26:59 +0300

    feat: add function make

---
+++ make.js
@@ -0,0 +1,11 @@
+function make(...args) {
+  const arr = [...args];
+  function inner(...args2) {
+    if (typeof args2[0] !== "function") {
+      arr.push(...args2);
+      return inner;
+    }
+    return arr.reduce(args2[0]);
+  }
+  return inner;
+}
