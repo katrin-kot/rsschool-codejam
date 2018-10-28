1e933f38aeebad43372b9c652b5d9590d734a114
katrin-kot <enkor89@mail.ru>
2018-10-28 20:43:25 +0300

    feat: add function recursion

---
+++ recursion.js
@@ -0,0 +1,14 @@
+function recursion(tree) {
+  const arr = [];
+  function preOrder(node, level) {
+    if (node == null) return;
+    if (arr[level] === undefined) {
+      arr[level] = [];
+    }
+    arr[level].push(node.value);
+    preOrder(node.left, level + 1);
+    preOrder(node.right, level + 1);
+  }
+  preOrder(tree, 0);
+  return arr;
+}
